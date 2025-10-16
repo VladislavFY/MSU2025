360a2af0805f4bf078be9b24a8fc4c548cbae8b5
VladislavFY <vladislavfrolov2008@gmail.com>
2025-10-16 11:54:16 +0300

    Update prototypes.h

--- task4_sort/prototypes.h
+++ task4_sort/prototypes.h
@@ -1,3 +1,7 @@
+#ifndef SUM_FUNC
+#define SUM_FUNC
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,3 +11,6 @@ double *generate_random_array(int len);
 void insertion_sort(double *array, int len);
 int is_sorted(const double *array, int len);
 void time_sort_and_report(double *array, int len);
+
+
+#endif
