c1d099ccd559afaf204a83f1ba431f52ee10bb5c
VladislavFY <vladislavfrolov2008@gmail.com>
2025-10-16 11:54:46 +0300

    Update prototypes.h

--- task4_sort/prototypes.h
+++ task4_sort/prototypes.h
@@ -1,10 +1,13 @@
 #ifndef SUM_FUNC
 #define SUM_FUNC
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define eps 1.e-10
+
+#define N 10.
 
 double *make_array(FILE*f, int *len); /* НЕ МЕНЯТЬ эту функцию — используется как есть */
 double *generate_random_array(int len);
