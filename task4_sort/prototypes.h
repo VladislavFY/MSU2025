6a06585102ea1459207b048bd12766dcb9d632cb
VladislavFY <vladislavfrolov2008@gmail.com>
2025-10-16 11:53:46 +0300

    Update prototypes.h

--- task4_sort/prototypes.h
+++ task4_sort/prototypes.h
@@ -1,3 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+double *make_array(FILE*f, int *len); /* НЕ МЕНЯТЬ эту функцию — используется как есть */
+double *generate_random_array(int len);
+void insertion_sort(double *array, int len);
+int is_sorted(const double *array, int len);
+void time_sort_and_report(double *array, int len);
